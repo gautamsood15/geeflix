ac9b8a31a78d9f4abb8349c5ca2dfc020277e913
gautamsood15 <gauty22@gmail.com>
2020-11-04 05:11:01 +0530

    outputting the episodes

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -19,8 +19,16 @@ class SeasonProvider {
       foreach($seasons as $season) {
         $seasonNumber = $season->getSeasonNumber();
 
+        $videosHtml = "";
+        foreach ($season->getVideos() as $video) {
+          $videosHtml .= $this->createVideoSquare($video);
+        }
+
         $seasonsHtml .= "<div class='season'>
                           <h3>Season $seasonNumber</h3>
+                          <div class='videos'>
+                            $videosHtml
+                          </div>
                         </div>";
       }
 
@@ -28,12 +36,29 @@ class SeasonProvider {
     }
 
 
-    private function createVideoSquare() {
+    private function createVideoSquare($video) {
       $id = $video->getId();
       $thumbnail = $video->getThumbnail();
-      $name = $video->getName();
+      $name = $video->getTitle();
       $description = $video->getDescription();
       $episodeNumber = $video->getEpisodeNumber();
+
+      return "<a href='watch.php?id=$id'>
+                <div class='episodeContainer'>
+                  <div class='contents'>
+
+                    <img src='$thumbnail'>
+
+                    <div class='videoInfo'>
+                      <h4>$name</h4>
+                      <span>$description</span>
+                    </div>
+
+                  </div>
+              
+                </div>
+      
+              </a>";
     }
 }
 
