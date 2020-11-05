f131c813465feb84b8bd30a416098bc8b82aa268
gautamsood15 <gauty22@gmail.com>
2020-11-05 19:02:02 +0530

    validating the first name

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,10 +1,18 @@
 <?php
 
 class Account {
+    
     private $con;
+    private $errorArray = array();
 
     public function __construct($con) {
         $this->con = $con;
+    }
+
+    public function validateFirstName($fn) {
+        if(strlen($fn) < 2 || strlen($fn) > 25) {
+            array_push($this->errorArray, "First name wrong length");
+        }
     }
 }
 ?>
