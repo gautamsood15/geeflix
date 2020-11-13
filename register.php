ce5e4e3e23d99d87bc0c1fa8c01465e30fd47641
gautamsood15 <gauty22@gmail.com>
2020-11-13 20:54:42 +0530

    Creating the register page

---
+++ register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Geeflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+        
+    </head>
+    <body>
+        hotdogs
+    </body>
+</html>
