abdb7f072d4a9a9d0c48360ddf6125aac5a14aa0
gautamsood15 <gauty22@gmail.com>
2020-11-14 18:01:01 +0530

    Connecting to the database

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,13 @@ ob_start(); //Turns on output buffering
 session_start();
 
 date_default_timezone_set("Indian/Mahe");
+
+try {
+    $con = new PDO("mysql:dbname=geeflix;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
+
 ?>
