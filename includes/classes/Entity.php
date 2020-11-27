a094bfcbb7c3be8b1a95b2a96f38b1e6cbf863ce
gautamsood15 <gauty22@gmail.com>
2020-11-28 03:09:58 +0530

    creating the video class

--- includes/classes/Entity.php
+++ includes/classes/Entity.php
@@ -38,6 +38,16 @@ class Entity {
         $query = $this->con->prepare("SELECT * FROM videos WHERE entityId=:id AND isMovie=0 ORDER BY season, episode ASC");
         $query->bindValue(":id", $entity->getId());
         $query->execute();
+
+        $seasons = array();
+        $videos = array();
+        $currentSeason = null;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            
+            
+            $currentSeason = $row["season"];
+            $videos[] = new Video($this->con, $row);
+        }
     }
 
 }
