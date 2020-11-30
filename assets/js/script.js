2d074e28542a0161023dc5192af325701d0c315a
gautamsood15 <gauty22@gmail.com>
2020-12-01 05:15:50 +0530

    marking the video as seen

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,7 @@ function updateProgressTimer(videoId, username) {
         }, 3000);
     })
     .on("ended", function() {
+        setFinished(videoId, username);
         window.clearInterval(timer);
     })
 }
@@ -61,6 +62,16 @@ function addDuration(videoId, username) {
 
 function updateProgress(videoId, username, progress) {
     $.post("ajax/updateDuration.php", { videoId: videoId, username: username, progress: progress }, function(data) {
+        if (data !== null && data !== "") {
+            alert(data);    
+        }
+        
+    });   
+}
+
+
+function setFinished(videoId, username) {
+    $.post("ajax/setFinished.php", { videoId: videoId, username: username }, function(data) {
         if (data !== null && data !== "") {
             alert(data);    
         }
