b8e8a4e0995755bedf27d409666a38d175879d02
gautamsood15 <gauty22@gmail.com>
2020-11-30 17:09:18 +0530

    up next video when there is no more episodes

--- includes/classes/VideoProvider.php
+++ includes/classes/VideoProvider.php
@@ -9,6 +9,16 @@ class VideoProvider {
         $query->bindData(":videoId", $currentVideo->getId());
 
         $query->execute();
+
+        if ($query->rowCount() == 0) {
+            $query = $con->prepare("SELECT * FROM videos WHERE season <=1 AND episode <=1 AND id != :videoId ORDER BY views DESC LIMIT 1");
+            
+            $query->bindValue(":videoId", $currentVideo->getId());
+            $query->execute();
+        }
+
+        $row = $query->fetch(PDO::FEtCH_ASSOC);
+        return new Video($con, $row);
     }
 }
 
