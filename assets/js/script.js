f4d13dab42dd37b7b43abf59b3cb71f89cce453c
gautamsood15 <gauty22@gmail.com>
2020-12-01 05:30:30 +0530

    resume playing where we left off

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ function startHideTimer() {
 
 function initVideo(videoId, username) {
     startHideTimer();
+    setStartTime(videoId, username);
     updateProgressTimer(videoId, username);
 }
 
@@ -76,5 +77,20 @@ function setFinished(videoId, username) {
             alert(data);    
         }
         
+    });   
+}
+
+
+function setStartTime(videoId, username) {
+    $.post("ajax/getProgress.php", { videoId: videoId, username: username }, function(data) {
+        if(isNaN(data)) {
+            alert(data);
+            return;
+        }
+
+        $("video").on("canplay", function() {
+            this.currentTime = data;
+            $("video").off("canplay");
+        })
     });   
 }
