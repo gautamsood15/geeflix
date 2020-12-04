3cde355e1460e60e17a5a70d510f1a732c1cbaa2
gautamsood15 <gauty22@gmail.com>
2020-12-04 07:15:01 +0530

    creating the search page html

---
+++ search.php
@@ -0,0 +1,9 @@
+<?php
+include_once("includes/header.php");
+?>
+
+<div class="textboxContainer">
+    <input type="text" class="searchInput" placeholder="Search for Something">
+</div>
+
+<div class="results"></div>
