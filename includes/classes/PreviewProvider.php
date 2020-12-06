0578ebcb51af2154b06464d9032b7f5c4c7e3714
gautamsood15 <gauty22@gmail.com>
2020-12-07 01:31:59 +0530

    Play button text

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -23,6 +23,9 @@ class PreviewProvider {
         $videoId = VideoProvider::getEntityVideoForUser($this->con, $id, $this->username);
         $video = new Video($this->con, $videoId);
 
+        $inProgress = $video->isInProgress($this->$username);
+        $playButtonText = $inProgress ? "Continue watching" : "Play";
+
         $seasonEpisode = $video->getSeasonAndEpisode();
         $subHeading = $video->isMovie() ? "" : "<h4>$seasonEpisode</h4>";
 
@@ -42,7 +45,7 @@ class PreviewProvider {
                             <h3>$name</h3>
                             $subHeading
                             <div class='buttons'>
-                                <button onclick='watchVideo($videoId)'><i class='fas fa-play'></i>  Play</button>
+                                <button onclick='watchVideo($videoId)'><i class='fas fa-play'></i>$playButtonText</button>
                                 <button onclick='volumeToggle(this)'><i class='fas fa-volume-mute'></i></button>
                             </div>
 
