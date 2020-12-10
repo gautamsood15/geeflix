2d881edf571517654993b97d48409a124d5586b6
gautamsood15 <gauty22@gmail.com>
2020-12-10 05:36:02 +0530

    Movies page categories

--- movies.php
+++ movies.php
@@ -5,6 +5,6 @@ $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createMoviesPreviewVideo();
 
 $containers = new CategoryContainers($con, $userLoggedIn);
-echo $containers->showTVShowCategories();
+echo $containers->showMoviesCategories();
 
 ?>
