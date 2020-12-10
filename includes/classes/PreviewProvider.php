45ea33e33fcb04e30ffb7fa1c369b22a588edb16
gautamsood15 <gauty22@gmail.com>
2020-12-11 02:21:23 +0530

    TV show preview video

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -9,7 +9,13 @@ class PreviewProvider {
     }
 
     public function createTVShowPreviewVideo() {
-        
+        $entitiesArray = EntityProvider::getTVShowEntities($this->con, null, 1);
+
+        if (sizeof($entitiesArray) == 0) {
+            ErrorMessage::show("No TV shows to display");
+        }
+
+        return $this->createPreviewVideo($entitiesArray[0]);
     }
 
     public function createPreviewVideo($entity) {
